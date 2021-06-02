268848faea457086f5ec4400088314f08eb8a5d5
sviatoslav <fryb08179@gmail.com>
2021-06-02 15:57:55 +0300

    First task

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 const rootElement = document.getElementById('root');
+/*
+  Добавить необходимые методы в класс ItemsContainer:
+    1. Кнопка активна только если в поле ввода есть текст без цифр (нужно регулярное выражение)
+    2. При нажатии на кнопку, добавляется в список значение поля ввода и очищается поле ввода
+*/
+class ItemsContainer {
+  get htmlElement() {
+    return rootElement.getElementById('itemsContainer');
+  }
+}
+
+const itemsContainer = new ItemsContainer();
